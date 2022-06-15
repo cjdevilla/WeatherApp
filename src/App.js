da21dae9a38e97025818bf412bed2d14cf36c5ae
Camille Joy De Villa <devillacamillejoym@gmail.com>
2022-06-15 20:46:23 +0800

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function App() {
+  // const url = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=e3f55693f92ecd1fa0081ad7f8b10a38`
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
