eefe3ab3983c0d3509694901d223147f625fa1a3
Camille Joy De Villa <devillacamillejoym@gmail.com>
2022-06-16 07:00:33 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
-  // const url = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=e3f55693f92ecd1fa0081ad7f8b10a38`
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=e3f55693f92ecd1fa0081ad7f8b10a38`;
 
   return (
-    <div>
-      <div></div>
+    <div className="app">
+      <div className="container"></div>
+      <div className="top">
+        <div className="location">
+          <p>Dallas</p>
+        </div>
+        <div className="temp">
+          <h1>65F</h1>
+        </div>
+        <div className="description">
+          <p>Clouds</p>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="feels">
+          <p className="bold">60F</p>
+          <p>Feels Like</p>
+        </div>
+        <div className="humidity">
+          <p>Humidity</p>
+        </div>
+        <div className="wind">
+          <p className="bold">12 MPH</p>
+          <p>Wind Speed</p>
+        </div>
+      </div>
     </div>
   );
 }
